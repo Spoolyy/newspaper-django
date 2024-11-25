776890beeb6e807d7bfa4f927f11c05014c7e6ab
Spoolyy <alessiosimeone3@gmail.com>
2024-11-25 19:09:38 +0100

    Latest

--- newspaper/newspaper/settings.py
+++ newspaper/newspaper/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
